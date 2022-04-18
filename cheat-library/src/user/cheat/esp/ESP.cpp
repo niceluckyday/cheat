3f74a9367a7243d2ac3764ba552e31d33c4a9e72
m0nkrel <m0nkrelino@gmail.com>
2022-04-18 18:25:19 +0300

    Fix case sensetive search

--- cheat-library/src/user/cheat/esp/ESP.cpp
+++ cheat-library/src/user/cheat/esp/ESP.cpp
@@ -121,7 +121,12 @@ namespace cheat::feature
 		
 		for (auto& info : filters)
 		{
-			if (info.first->value().m_Name.find(m_Search) != std::string::npos) 
+			//m0nrkel : We making a string copies and lowercase them to avoid case sensitive search
+			std::string name = info.first->value().m_Name;
+			std::transform(name.begin(), name.end(), name.begin(), ::tolower);
+			std::string search = m_Search;
+			std::transform(search.begin(), search.end(), search.begin(), ::tolower);
+			if (name.find(search) != std::string::npos) 
 				validFilters.push_back(&info);
 		}
 
