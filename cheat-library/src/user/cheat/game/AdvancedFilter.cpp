d63213c7b9a4d7b55fe9cb2dc00f6bc1abd04a7a
CallowBlack <stroganov-oleges@yandex.ru>
2022-04-20 11:21:38 +0300

    refactor

--- cheat-library/src/user/cheat/game/AdvancedFilter.cpp
+++ cheat-library/src/user/cheat/game/AdvancedFilter.cpp
@@ -3,54 +3,31 @@
 
 namespace cheat::game
 {
+	AdvancedFilter::AdvancedFilter(const std::vector<app::EntityType__Enum_1>& types, const std::vector<std::string>& names)
+		: m_Names(names), m_Types(types) { }
 
 	bool AdvancedFilter::IsValid(Entity* entity) const 
 	{
 		if (entity == nullptr)
 			return false;
 
-
-		if (m_Types.size() == 0)
-			return true;
-
-		bool found = false;
-		for (auto& type : m_Types)
-		{
-			if (entity->type() == type)
-			{
-				found = true;
-				break;
-			}					
-		}if(found == false)
+		bool type_found = m_Types.size() == 0 || std::any_of(m_Types.cbegin(), m_Types.cend(),
+			[entity](const app::EntityType__Enum_1& type) { return entity->type() == type; });
+		
+		if (!type_found)
 			return false;
-		
-		
+
 		auto& name = entity->name();
-		for (auto& pattern : m_Names)
-		{
-			if (name.find(pattern) != -1)
-				return true;
-		}
+		bool name_found = m_Names.size() == 0 || std::any_of(m_Names.cbegin(), m_Names.cend(),
+			[name](const std::string& pattern) { return name.find(pattern) != std::string::npos; });
 		
-
-		return false;
-	}
-
-	AdvancedFilter::AdvancedFilter(const std::vector<app::EntityType__Enum_1>& types, const std::vector<std::string>& names ) : m_Names(names), m_Types(types)
-	{
-
+		return name_found;
 	}
 
 	AdvancedFilter operator+(AdvancedFilter lFilter, const AdvancedFilter& rFilter)
 	{
 		lFilter.m_Names.insert(lFilter.m_Names.end(), rFilter.m_Names.begin(), rFilter.m_Names.end());
+		lFilter.m_Types.insert(lFilter.m_Types.end(), rFilter.m_Types.begin(), rFilter.m_Types.end());
 		return lFilter;
 	}
-	
-	AdvancedFilter operator+(AdvancedFilter lFilter, const std::vector<app::EntityType__Enum_1>& rFilter)
-	{
-		lFilter.m_Types.insert(lFilter.m_Types.end(), rFilter.begin(), rFilter.end());
-		return lFilter;
-	}
-	
 }
--- cheat-library/src/user/cheat/game/AdvancedFilter.h
+++ cheat-library/src/user/cheat/game/AdvancedFilter.h
@@ -10,12 +10,11 @@ namespace cheat::game
         public IEntityFilter
     {
     public:
-        //make filter
-        AdvancedFilter(const std::vector<app::EntityType__Enum_1>& types = {},const   std::vector<std::string>& names = {});
+        AdvancedFilter(const std::vector<app::EntityType__Enum_1>& types = {}, const std::vector<std::string>& names = {});
         AdvancedFilter(const app::EntityType__Enum_1 type, const std::string name) : AdvancedFilter(std::vector<app::EntityType__Enum_1>{type}, std::vector<std::string>{name}) {}
         bool IsValid(Entity* entity) const override;
         friend AdvancedFilter operator+(AdvancedFilter lFilter, const AdvancedFilter& rFilter);
-        friend AdvancedFilter operator+(AdvancedFilter lFilter, const std::vector<app::EntityType__Enum_1>& rFilter);
+
     private:
         std::vector<app::EntityType__Enum_1> m_Types;
         std::vector<std::string> m_Names;
